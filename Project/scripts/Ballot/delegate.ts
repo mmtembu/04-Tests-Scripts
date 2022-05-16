823469df025c7a94018df5e5793cdc1bf698b237
mmtembu <mthembumangaliso@gmail.com>
2022-05-16 14:26:49 +0200

    added deledate and vote

--- Project/scripts/Ballot/delegate.ts
+++ Project/scripts/Ballot/delegate.ts
@@ -1,13 +1,12 @@
-import { Contract, ethers } from "ethers";
+import { ethers, Contract } from "ethers";
 import "dotenv/config";
 import * as ballotJson from "../../artifacts/contracts/Ballot.sol/Ballot.json";
-// eslint-disable-next-line node/no-missing-import
 import { Ballot } from "../../typechain";
 
 // This key is already public on Herong's Tutorial Examples - v1.03, by Dr. Herong Yang
 // Do never expose your keys like this
 const EXPOSED_KEY =
-  "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
+  "a8b513369437e05aee54948867a86923858a71d5ac380e7db91fd9717e453909";
 
 async function main() {
   const wallet =
@@ -19,34 +18,33 @@ async function main() {
   const signer = wallet.connect(provider);
   const balanceBN = await signer.getBalance();
   const balance = Number(ethers.utils.formatEther(balanceBN));
+  const lastBlock = await provider.getBlock('latest');
+//   console.log(`Connected to ropsten network at height`)
+//   console.log(lastBlock);
   console.log(`Wallet balance ${balance}`);
   if (balance < 0.01) {
     throw new Error("Not enough ether");
   }
-  if (process.argv.length < 3) throw new Error("Ballot address missing");
-  const ballotAddress = process.argv[2];
-  if (process.argv.length < 4) throw new Error("Voter address missing");
-  const voterAddress = process.argv[3];
-  console.log(
-    `Attaching ballot contract interface to address ${ballotAddress}`
-  );
+  console.log("Delegating");
+  console.log("Delegating my vote...");
+  if(process.argv.length < 3) throw new Error("No user address provided")
+  const userAddress = process.argv[2];
+
+
+  console.log(`Voter address given as: ${userAddress}`);
+//   console.log(`Proposal given as: ${userAddress}`);
+
   const ballotContract: Ballot = new Contract(
-    ballotAddress,
+    userAddress,
     ballotJson.abi,
     signer
   ) as Ballot;
 
-  const chairpersonAddress = await ballotContract.chairperson();
-  if (chairpersonAddress !== signer.address)
-    throw new Error("Caller is not the chairperson for this contract");
-  console.log(`Giving right to vote to ${voterAddress}`);
-  const tx = await ballotContract.giveRightToVote(voterAddress);
-  console.log("Awaiting confirmations");
-  await tx.wait();
-  console.log(`Transaction completed. Hash: ${tx.hash}`);
+  ballotContract.delegate(userAddress);
 }
 
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
